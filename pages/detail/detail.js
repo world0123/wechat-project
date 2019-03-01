952d5b9cd95a4f25c9b86850dd590148f15aa404
liwhels <870169293@qq.com>
2019-03-01 22:41:48 +0800

    emmmm

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,7 +1,4 @@
 // pages/detail.js
-var that = this;
-const app = getApp()
-const util = require("../../utils/util.js")
 const db = wx.cloud.database()
 Page({
 
@@ -9,14 +6,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    datas:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function (params) {
+    var that = this
+    console.log(params.id)
+    db.collection('todos').doc(params.id).get({
+      success(res) {
+        console.log(res.data)//下次删除
+        that.setData({
+          datas: res.data,
+          success:function(){
+            console.log("设置成功")
+          }
+        })
+      }
+    })
   },
 
   /**
@@ -67,88 +76,4 @@ Page({
   onShareAppMessage: function () {
 
   },
-  drawStart: function (e) {
-    // console.log("drawStart");  
-    var touch = e.touches[0]
-
-    for (var index in that.data.data) {
-      var item = this.data.data[index]
-      item.right = 0
-    }
-    this.setData({
-      data: this.data.data,
-      startX: touch.clientX,
-    })
-
-  },
-  drawMove: function (e) {
-    var index = e.target.dataset.index
-    var touch = e.touches[0]
-    var item = this.data.data[index]
-    var disX = this.data.startX - touch.clientX
-    //设置背景图片的透明度
-    //console.log(disX)
-    if (disX >= 0) {
-      item.opacity_value = 1 - disX / 375
-      item.right = disX
-      this.setData({
-        data: this.data.data
-      })
-    } else {
-      item.right = 0
-      item.opacity_value = 1
-      this.setData({
-        isScroll: true,
-        data: this.data.data
-      })
-    }
-  },
-  drawEnd: function (e) {
-    var index = e.target.dataset.index
-    var list = this.data.data
-    var item = this.data.data[e.currentTarget.dataset.index]
-    var id = this.data.data[index]._id
-    //disX大于某个距离时弹出框
-    if (item.right >= 200) {
-      item.opacity_value = 0
-      wx.showModal({
-        title: '提示',
-        content: '确定删除此卡片',
-        success: function (res) {
-          if (res.confirm) {
-            list.splice(index, 1)
-            //删除云端数据
-            db.collection('todos').doc(id).remove({
-              success(res) {
-                console.log("删除成功")
-              }
-            })
-            //更新本地列表
-            this.setData({
-              data: list
-            });
-          }
-          else if (res.cancel) {
-            item.right = 0
-            item.opacity_value = 1
-            this.setData({
-              isScroll: true,
-              data: this.data.data,
-            })
-          }
-        }
-      })
-      this.setData({
-        isScroll: true,
-        data: this.data.data,
-      })
-    } else {
-      item.right = 0
-      item.opacity_value = 1
-      this.setData({
-        isScroll: true,
-        data: this.data.data,
-      })
-    }
-  }  
 })
--- pages/world0123/me.js
+++ pages/world0123/me.js
@@ -117,74 +117,79 @@ Page({
 
   },
   drawMove: function (e) {
-    var index = e.target.dataset.index
-    var touch = e.touches[0]
-    var item = this.data.data[index]
-    var disX = this.data.startX - touch.clientX
-    //设置背景图片的透明度
-    //console.log(disX)
-    if (disX >= 0) {
-      item.opacity_value = 1 - disX / 375
-      item.right = disX
-      this.setData({
-        data: this.data.data
-      })
-    } else {
-      item.right = 0
-      item.opacity_value = 1
-      this.setData({
-        isScroll: true,
-        data: this.data.data
-      })
-    }
+    if(e.target.id=="viewx"){
+      var index = e.target.dataset.index
+      var touch = e.touches[0]
+      var item = this.data.data[index]
+      var disX = this.data.startX - touch.clientX
+      //console.log(e)
+      //设置背景图片的透明度
+      //console.log(disX)
+      if (disX >= 0) {
+        item.opacity_value = 1 - disX / 375
+        item.right = disX
+        this.setData({
+          data: this.data.data
+        })
+      } else {
+        item.right = 0
+        item.opacity_value = 1
+        this.setData({
+          isScroll: true,
+          data: this.data.data
+        })
+      }
+  }
   },
   drawEnd: function (e) {
-    var that = this
-    var index = e.target.dataset.index
-    var list = that.data.data
-    var item = that.data.data[e.currentTarget.dataset.index]
-    var id = that.data.data[index]._id
-    //disX大于某个距离时弹出框
-    if (item.right >= 200) {
-      item.opacity_value = 0
-      wx.showModal({
-        title: '提示',
-        content: '确定删除此卡片',
-        success: function (res) {
-          if (res.confirm) {
-            list.splice(index, 1)
-            //删除云端数据
-            db.collection('todos').doc(id).remove({
-              success(res) {
-                console.log("删除成功")
-              }
-            })
-            //更新本地列表
-            that.setData({
-              data: list
-            });
+    if (e.target.id == "viewx"){
+      var that = this
+      var index = e.target.dataset.index
+      var list = that.data.data
+      var item = that.data.data[e.currentTarget.dataset.index]
+      var id = that.data.data[index]._id
+      //disX大于某个距离时弹出框
+      if (item.right >= 200) {
+        item.opacity_value = 0
+        wx.showModal({
+          title: '提示',
+          content: '确定删除此卡片',
+          success: function (res) {
+            if (res.confirm) {
+              list.splice(index, 1)
+              //删除云端数据
+              db.collection('todos').doc(id).remove({
+                success(res) {
+                  console.log("删除成功")
+                }
+              })
+              //更新本地列表
+              that.setData({
+                data: list
+              });
+            }
+            else if (res.cancel) {
+              item.right = 0
+              item.opacity_value = 1
+              that.setData({
+                isScroll: true,
+                data: that.data.data,
+              })
+            }
           }
-          else if (res.cancel) {
-            item.right = 0
-            item.opacity_value = 1
-            that.setData({
-              isScroll: true,
-              data: that.data.data,
-            })
-          }
-        }
-      })
-      that.setData({
-        isScroll: true,
-        data: that.data.data,
-      })
-    } else {
-      item.right = 0
-      item.opacity_value = 1
-      that.setData({
-        isScroll: true,
-        data: that.data.data,
-      })
+        })
+        that.setData({
+          isScroll: true,
+          data: that.data.data,
+        })
+      } else {
+        item.right = 0
+        item.opacity_value = 1
+        that.setData({
+          isScroll: true,
+          data: that.data.data,
+        })
+      }
     }
   }
 })
